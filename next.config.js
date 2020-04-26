e2e971b93c174518541c20d5f308de76ead80dc9
anthonyDrury <anthony.drury1@gmail.com>
2020-04-26 14:57:30 +1000

    fix: environment variables

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   pageExtensions: ["page.tsx"],
   distDir: "build",
+  env: {
+    REACT_APP_ANALYTICS_KEY=$REACT_APP_ANALYTICS_KEY,
+    REACT_APP_API_URL=$REACT_APP_API_URL
+  }
 };
